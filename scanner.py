dd32d2312c2163f0c13d57f54a754c3ad054a2e2
Alyssia Tavares <alyssiatavares@gmail.com>
2025-04-22 07:50:35 -0700

    portscanner

--- scanner.py
+++ scanner.py
@@ -1,47 +1,58 @@
 import nmap
 scanner = nmap.PortScanner()
 
-print("Seja bem vindo ao Scanner!")
+print("Welcome to Scanner!")
 print(30*"-")
-ip = input("Digite o IP ou URL: ")
-print("O ip digitado foi: ", ip)
+ip = input("Enter IP or URL: ")
+print("The IP entered was:", ip)
 type(ip)
 
-menu = input("""\n Escolha o tipo de scan a ser realizado:
-      1 - Varredura do tipo SYN Scan
-      2 - Varredura do tipo UDP Scan
-      3- Varredura do tipo intensa
-      Digite o número correspondente a opção desejada: """)
+menu = input("""\n Choose the type of scan to be performed:
+      1 - SYN Scan type scan
+      2 - UDP Scan type scan
+      3- Intense type scan
+      Enter the number corresponding to the desired option: """)
 
-print("A opção escolhida foi: ", menu)  
+print("The chosen option was:", menu)  
 
 if menu == "1":
-    print("Versao do Nmap: ", scanner.nmap_version())
+    print("Nmap version: ", scanner.nmap_version())
     scanner.scan(ip, '1-1024', '-v -sS')
     print(scanner.scaninfo())
-    print("Status do IP: ", scanner[ip].state())
-    print(scanner[ip].all_protocols())
-    print(" ")
-    print("Portas abertas: ", scanner[ip]['tcp'].keys())
+
+    if ip in scanner.all_hosts():
+        print("IP Status: ", scanner[ip].state())
+        print(scanner[ip].all_protocols())
+        print(" ")
+        print("Open doors: ", scanner[ip]['tcp'].keys())
+    else:
+        print("❌ Host not found or did not respond to the scan.")
    
 elif menu == "2":
-    print("Versao do Nmap: ", scanner.nmap_version())
+    print("Nmap version: ", scanner.nmap_version())
     scanner.scan(ip, '1-1024', '-v -sU')
     print(scanner.scaninfo())
-    print("Status do IP: ", scanner[ip].state())
-    print(scanner[ip].all_protocols())
-    print(" ")
-    print("Portas abertas: ", scanner[ip]['udp'].keys())
+    if ip in scanner.all_hosts():
+        print("IP Status: ", scanner[ip].state())
+        print(scanner[ip].all_protocols())
+        print(" ")
+        print("Open doors: ", scanner[ip]['udp'].keys())
+    else:
+        print("❌ Host not found or did not respond to the scan.")
     
 elif menu == "3":
-    print("Vesao do Nmap: ", scanner.nmap_version())
-    scanner.scan(ip,'1-1024', '-v -sC')
-    print("Status do IP: ", scanner[ip].state())
-    print(scanner[ip].all_protocols())
-    print(" ")
-    print("Portas abertas: ", scanner[ip]['tcp'].keys())
-
+    print("Nmap version: ", scanner.nmap_version())
+    scanner.scan(ip,'1-1024', '-v -sC -sV')
+    print(scanner.scaninfo())
+    if ip in scanner.all_hosts():
+        print("IP Status: ", scanner[ip].state())
+        print(scanner[ip].all_protocols())
+        print(" ")
+        print("Open doors: ", scanner[ip]['tcp'].keys())
+    else:
+        print("❌ Host not found or did not respond to the scan.")
 else:
-    print("Opção inválida!")
-    print("Tente novamente.")
+    print("Invalid option!")
+    print("Please, try again.")
     exit(0)
+
